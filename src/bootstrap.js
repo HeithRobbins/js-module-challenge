c5c4100b6bfb429d58a7789a8644ac548445fb30
Heith Robbins <heith3214@gmail.com>
2020-02-13 09:03:50 -0700

    fixed to have all other items

---
+++ src/bootstrap.js
@@ -0,0 +1,5 @@
+import classChallenge, { greeting, sum } from "./classchallenge";
+
+console.log(classChallenge)
+console.log(greeting)
+console.log(sum(300, 200))
---
+++ src/classchallenge.js
@@ -0,0 +1,9 @@
+export default function () {
+    return '<div>classChallenge</div>'
+}
+
+const greeting = 'welcome'
+
+export function sum(numOne, numTwo) {
+    return numOne + numTwo
+}
